8984e94930e59a3ce635b9fb2a5484cc6df38663
defrell01 <sashka.sahkov@gmail.com>
2022-10-23 14:18:59 +0700

    front-end auth 50% ready

---
+++ client/src/http/index.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const API_URL = 'http://localhost:5000'
+
+const $api = axios.create({
+    withCredentials: true,
+    baseURL: API_URL
+})
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    return config
+})
+
+export default $api;
